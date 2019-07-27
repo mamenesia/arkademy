9d65f288513f8f4c4f25ad691a3c4447e7a6e34a
mamenesia <dedy.prasetyo.h@gmail.com>
2019-07-27 09:32:56 +0700

    finish soal1

---
+++ soal1.js
@@ -0,0 +1,101 @@
+const myBiodata = () => {
+    return {
+        'name': 'Dedy Prasetyo Hermawan',
+        'age' : 25,
+        'address' : 'Yogyakarta',
+        'hobbies' : ['Swimming', 'Programming', 'Game', 'Reading', 'Jogging', 'Hiking'],
+        'is_married' : false,
+        'list_school' : [
+                            {
+                                name : 'Universitas Gadjah Mada',
+                                year_in : 2012,
+                                year_out : 2016,
+                                major : 'Teknik Nuklir'
+                            },
+
+                            {
+                                name : 'SMAN 1 Praya',
+                                year_in : 2009,
+                                year_out : 2012,
+                                major : 'IPA'
+                            },
+
+                            {
+                                name : 'SMPN 1 Praya',
+                                year_in : 2006,
+                                year_out : 2009,
+                                major : null
+                            },
+
+                            {
+                                name : 'SDN 5 Praya',
+                                year_in : 2002,
+                                year_out : 2006,
+                                major : null
+                            }
+                        ],
+        'skills' :  [
+                        {
+                            skill_name : 'Nuclear Simulation',
+                            level : 'expert'
+                        },
+
+                        {
+                            skill_name : 'English',
+                            level : 'advanced'
+                        },
+
+                        {
+                            skill_name : 'Search Engine Optimization',
+                            level : 'advanced'
+                        },
+
+                        {
+                            skill_name : 'Flutter',
+                            level : 'advanced'
+                        },
+
+                        {
+                            skill_name : 'HTML & CSS',
+                            level : 'advanced'
+                        },
+
+                        {
+                            skill_name : 'Javascript',
+                            level : 'advanced'
+                        },
+
+                        {
+                            skill_name : 'Sass',
+                            level : 'advanced'
+                        },
+
+                        {
+                            skill_name : 'NodeJS',
+                            level : 'beginner'
+                        },
+
+                        {
+                            skill_name : 'Python',
+                            level : 'beginner'
+                        },
+
+                        {
+                            skill_name : 'CorelDraw',
+                            level : 'beginner'
+                        },
+
+                        {
+                            skill_name : 'MySQL',
+                            level : 'beginner'
+                        },
+
+                        {
+                            skill_name : 'MongoDB',
+                            level : 'beginner'
+                        }
+                    ],
+        "interest_in_coding" : true
+        
+    }
+};
