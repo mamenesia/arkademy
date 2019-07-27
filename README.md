609ede0df0e8076f8815aa666b47bfbfa6aed1e2
mamenesia <dedy.prasetyo.h@gmail.com>
2019-07-27 20:33:32 +0700

    complete post method

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # arkademy
+
+## Soal No. 1
+ * Kegunaan JSON pada REST API adalah sebagai format untuk mengirim (sending) dan meminta (requesting) data pada REST API. Selain JSON format data transfer bisa berupa XML (eXtensible Markup Language) dan plain text, tetapi format data JSON lebih populer karena berukuran ringan dan formatnya lebih mudah untuk dibaca dan dimengerti.
+
+## Soal No. 2
+  * 
--- soal6/routes/index.js
+++ soal6/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var db = mysql.createPool({
+  host : 'localhost',
+  user : 'root',
+  password : '',
+  database : 'test_arkademy',
+  debug : false
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.query('SELECT name.name, work.work, category.salary FROM name, work, category WHERE name.id_work=work.id AND name.id_salary=category.id;', (err, rs) =>{
+    res.render('index', { title: 'Arkademy Bootcamp', data: rs });
+  });
+});
+
+
+router.get('/form', (req,res,next) => {
+  db.query('SELECT work.work, category.salary, work.id_salary, category.id FROM work, category WHERE work.id_salary=category.id', (err, rs) =>{
+    res.render('form', { work: rs });
+  });
+});
+
+router.post('/form', (req,res,next) => {
+  db.query('INSERT INTO name SET ?', req.body, (err, rs) =>{
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
