3ad1a1fb988b52909c9c838d5c741faa6ffc20a7
mamenesia <dedy.prasetyo.h@gmail.com>
2019-07-27 21:44:19 +0700

    complete soal 6C

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# arkademy
+# Arkademy Bootcamp
 
 ## Soal No. 1
  * Kegunaan JSON pada REST API adalah sebagai format untuk mengirim (sending) dan meminta (requesting) data pada REST API. Selain JSON format data transfer bisa berupa XML (eXtensible Markup Language) dan plain text, tetapi format data JSON lebih populer karena berukuran ringan dan formatnya lebih mudah untuk dibaca dan dimengerti.
--- soal6/routes/index.js
+++ soal6/routes/index.js
@@ -11,20 +11,38 @@ var db = mysql.createPool({
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  db.query('SELECT name.name, work.work, category.salary FROM name, work, category WHERE name.id_work=work.id AND name.id_salary=category.id;', (err, rs) =>{
+  db.query('SELECT name.id, name.name, work.work, category.salary FROM name, work, category WHERE name.id_work=work.id AND name.id_salary=category.id;', (err, rs) =>{
     res.render('index', { title: 'Arkademy Bootcamp', data: rs });
   });
 });
 
-
 router.get('/form', (req,res,next) => {
-  db.query('SELECT work.work, category.salary, work.id_salary, category.id FROM work, category WHERE work.id_salary=category.id', (err, rs) =>{
-    res.render('form', { work: rs });
+  db.query('SELECT name.name, work.work, category.salary, work.id_salary, category.id FROM name, work, category WHERE work.id_salary=category.id', (err, rs) =>{
+    res.render('form', { work : rs, name : {} });
   });
 });
 
 router.post('/form', (req,res,next) => {
   db.query('INSERT INTO name SET ?', req.body, (err, rs) =>{
+    res.redirect('/');
+  });
+});
+
+router.get('/delete', (req, res, next)=>{
+  db.query('DELETE FROM name WHERE id=?', req.query.id, (err, rs)=>{
+    res.redirect('/');
+  });
+});
+
+router.get('/edit', (req, res, next) => {
+  db.query('SELECT name.id, name.name, work.work, category.salary, work.id_salary FROM name, work, category WHERE work.id_salary=category.id AND name.id=?', req.query.id, (err, rs)=>{
+    res.render('form', { name : rs[0], work : rs});
+  });
+});
+
+router.post('/edit', (req, res, next) =>{
+  var param = [ req.body, req.query.id]
+  db.query('UPDATE name SET ? WHERE id=?', param, (err, rs) => {
     res.redirect('/');
   });
 });
