d16869b82ede55f88e9fcbcd98d9f9f528386c61
mamenesia <dedy.prasetyo.h@gmail.com>
2019-07-27 10:33:55 +0700

    refactoring soal2

--- soal2.js
+++ soal2.js
@@ -2,3 +2,9 @@ const is_username_valid = (username) =>  /^[a-zA-Z][\w]{4,9}$/.test(username);
 
 console.log(is_username_valid('Xrutaf888'));
 console.log(is_username_valid('1Diana'));
+
+
+const is_password_valid = (password) =>  /(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[\W|_])(?=.*[=]).{8,}$/.test(password);
+
+console.log(is_password_valid('passW0rd='));
+console.log(is_password_valid('C0d3YourFuture!#'));
